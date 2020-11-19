7bd339d88bda17072c786f0bce1351333e4380c5
steven huynh <steven.h@live.com>
2020-11-18 17:46:28 -0800

    Modified Node class and add Knight class Changes made to Node: - added instance variable visited along with the attr_accessor Knight class: - 2 methods added:   - piece_move_one_step_from     - gets all possible legal position a knight can move at a given spot   - knight_moves     - traverses the board using above method until destination is found       - horrible, not-so-shortest-path implementation       - will change

--- knights-travails.rb
+++ knights-travails.rb
@@ -2,10 +2,11 @@ require 'set'
 require 'pry'
 
 class Node
-  attr_accessor :value, :adjacency_list
+  attr_accessor :value, :adjacency_list, :visited
   def initialize(value)
     @value = value
     @adjacency_list = Set.new
+    @visited = false
   end
 end
 
@@ -39,8 +40,62 @@ class Board
   end
 end
 
+class Knight
+  attr_accessor :board
+  def initialize
+    @board = Board.new.board
+  end
+
+  def piece_move_one_step_from(start)
+    return nil unless board.has_key? start
+
+    x = start[0]
+    y = start[1]
+    up_left = [x - 2, y - 1]
+    up_right = [x - 2, y + 1]
+    down_left = [x + 2, y - 1]
+    down_right = [x + 2, y + 1]
+    left_up = [x - 1, y - 2]
+    left_down = [x + 1, y - 2]
+    right_up = [x - 1, y + 2]
+    right_down = [x + 1, y + 2]
+    destinations = []
+    [up_left, up_right, down_left, down_right, left_up, left_down, right_up, right_down].each do |coord|
+      destinations << coord if board.has_key? coord
+    end
+    destinations.map! { |coord| coord = board[coord] }.uniq!
+    destinations
+  end
+
+  def knight_moves(start, dest)
+    # remove ele from front: shift
+    board[start].visited = true
+    steps = [start]
+    queue = piece_move_one_step_from start
+    until queue.empty?
+      processed_node = queue.shift
+      steps << processed_node.value unless processed_node.visited
+      processed_node.visited = true
+      break if processed_node.value == dest
+
+      queue += piece_move_one_step_from processed_node.value
+    end
+    steps
+  end
+end
+
 b = Board.new
-b.board.each_value do |ele|
-  print "node: #{ele.value}, list: #{ele.adjacency_list}"
+(0..7).each do |x|
+  (0..7).each do |y|
+    print b.board[[x, y]].value
+  end
   puts
 end
+# b.board.each_value do |ele|
+#   print "node: #{ele.value}, list: #{ele.adjacency_list}"
+#   puts
+# end
+k = Knight.new
+# p k.piece_move_one_step_from([0, 0])
+s = k.knight_moves([0, 0], [7, 7])
+s.each { |s| p s; puts }
